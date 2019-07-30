dbd6e30edef6fab8a4bee001f0dfb6384c819f99
unknown <diefrodriguezcha@unal.edu.co>
2019-07-30 11:48:20 -0500

    Now running outside intranet. Make manuals

--- src/assets/js/variables.ts
+++ src/assets/js/variables.ts
@@ -1,4 +1,4 @@
-export var url:string = 'https://40c2dd6f.ngrok.io';
+export var url:string = 'https://a551edbf.ngrok.io';
 
 export var empresasLogos = {
     'ACERIAS DE COLOMBIA S.A.S': 'Acesco',
