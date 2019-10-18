8f5e8b83136c65da9a8229c0c9c39774f78bed49
nicolasenciso <nricardoe@unal.edu.co>
2019-10-18 09:39:18 -0500

    alineado estilo completo editar asignacionW

--- src/assets/js/variables.ts
+++ src/assets/js/variables.ts
@@ -1,4 +1,4 @@
-export var url:string = 'https://0aac4db3.ngrok.io';
+export var url:string = 'https://4396fe7b.ngrok.io';
 
 export var empresasLogos = {
     'ACERIAS DE COLOMBIA S.A.S': 'Acesco',
