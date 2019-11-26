4bfbea2b52d3da07eb9bde1b324798c21bd507fd
nicolasenciso <nricardoe@unal.edu.co>
2019-11-26 09:50:56 -0500

    PM edit added

--- src/app/pages/main/field-service/edit-asignacion/edit-asignacion.component.ts
+++ src/app/pages/main/field-service/edit-asignacion/edit-asignacion.component.ts
@@ -23,6 +23,7 @@ export class EditAsignacionComponent implements OnInit {
   ResultadosEmpresas : JSON[];
   ResultadosField : JSON[];
   ResultadosStatus: JSON[];
+  ResultadosPM: JSON[];
   asignacionData;
 
   getIdAsignacion(url){
@@ -44,6 +45,7 @@ export class EditAsignacionComponent implements OnInit {
     var datos9 = document.forms["formulario"].elements[8].value;
     var datos10 = document.forms["formulario"].elements[9].value;
     var datos11 = document.forms["formulario"].elements[10].value;
+    var datos12 = document.forms["formulario"].elements[11].value;
     var IdEmpresa = this.asignacionData[0]['IdEmpresa'];
     var IdAsignacion = this.idAsignacion;
     var StatusAsignacion;
@@ -57,27 +59,36 @@ export class EditAsignacionComponent implements OnInit {
       StatusAsignacion = 0;
     }
 
+    var IdPM;
+    for (let i in this.ResultadosPM){
+      if (this.ResultadosPM[i]['NombrePM'] == datos4){
+        IdPM = this.ResultadosPM[i]['IdPM'];
+      }
+
+    }
+
     var datos = {"IdEspecialista" : datos3,
+                 "IdPM": IdPM,
                  "StatusAsignacion": StatusAsignacion,
                  "IdAsignacion" : IdAsignacion,
                  "IdEmpresa" : IdEmpresa,
-                 "IdStatus" : datos4,
+                 "IdStatus" : datos5,
                  "NombreCliente" : datos1,
                  "NombrePlanta" : datos2, 
                  "CiudadPlanta" : this.finalCoords[2],
-                 "PCFSV": datos5,
-                 "FechaInicio" : datos6,
-                 "FechaFin" : datos7,
+                 "PCFSV": datos6,
+                 "FechaInicio" : datos7,
+                 "FechaFin" : datos8,
                  "CoordenadasSitio" : this.finalCoords[0],
                  "NombreSitio" : this.infoUbicacion.split(",")[2],
-                 "NombreContacto" : datos8,
-                 "TelefonoContacto" : datos9,
-                 "EmailContacto" : datos10,
-                 "Descripcion" : datos11,
+                 "NombreContacto" : datos9,
+                 "TelefonoContacto" : datos10,
+                 "EmailContacto" : datos11,
+                 "Descripcion" : datos12,
                  "sameEspecialista" : sameEspecialista
                 };
 
-    //console.log(datos);
+    console.log(datos);
     if(this.infoUbicacion == ""){
       datos["CiudadPlanta"] = this.asignacionData[0]["CoordenadasSitio"];
     }
@@ -155,7 +166,10 @@ export class EditAsignacionComponent implements OnInit {
       else
       document.getElementById('ubicacion').textContent = this.finalCoords[1] + ', '+ this.finalCoords[2];
     });
-    
+      this.DataRetriever.getData(env.url+'/api/PM').then(data => {
+        this.ResultadosPM = data as JSON[];
+      })
+
       this.DataRetriever.getData(env.url+'/api/allWorkers').then(data => {
       this.ResultadosField = data as JSON[];
       })
